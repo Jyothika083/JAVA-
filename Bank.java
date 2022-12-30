a0727df5b027b9bf08824b38c12f6c01c4fcacc6
Jyothika083 <117371810+Jyothika083@users.noreply.github.com>
2022-12-30 11:48:24 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -5,12 +5,41 @@ class Account
 	String accno;
 	String type;
 	double balance;
-	void get_values(String cname,String acc,String typ,double bal)
+	double amt;
+	Scanner sc=new Scanner(System.in);
+	void get_values()
 	{
-		cust_name=cname;
-		accno=acc;
-		type=typ;
-		balance=bal;
+		
+		System.out.println("Enter customer name");
+		cust_name=sc.next();
+		System.out.println("Enter account number");
+		accno=sc.next();
+		System.out.println("Enter initial balance");
+		balance=s.nextDouble();
+	}
+	void depo()
+	{
+		System.out.println("Enter amount to be deposited");
+		amt=sc.nextDouble();
+		balance+=amt;
+	}
+	void withdraw()
+	{
+		System.out.println("Enter amount to be withdrawn");
+		amt=sc.nextDouble();
+		if(balance<=amt)
+			System.out.printl("Cannot withdraw");
+		else
+			balance-=amt;
+		
+	}
+	void display_bal()
+	{
+		if(balance<1000)
+		{
+			balance=balance-100;
+		}
+		System.out.println("Balance="+balance);
 	}
 }
 class Cur_acct extends Account
@@ -19,23 +48,10 @@ class Cur_acct extends Account
 	{
 		type="Current_account";
 	}
-	void depo(double amt)
-	{
-		balance+=amt;
-	}
 	
-	void display_bal()
-	{
-		if(balance<1000)
-		{
-			balance=balance-100;
-		}
-		System.out.println("Balance="+balance);
-	}
-	void withdraw(int amt)
-	{
-		balance-=amt;
-	}
+	
+	
+	
 }
 class Sav_acct extends Account
 {
@@ -43,24 +59,12 @@ class Sav_acct extends Account
 	{
 		type="Savings_account";
 	}
-	void depo(double amt)
-	{
-		balance+=amt;
-	}
-	void display_bal()
-	{
-		if(balance<1000)
-		{
-			balance=balance-100;
-		}
-		System.out.println("Balance="+balance);
-	}
-	void withdraw(int amt)
-	{
-		balance-=amt;
-	}
-	void interest(double r,double t,double n)
+	void interest()
 	{	
+		System.out.println("Enter rate of interest, time period and number of times interest is applied ");
+		double r==sc.nextDouble();
+		double t=sc.nextDouble();
+		double n=sc.nextDouble();
 		balance=balance*(math.pow((1+r/n),(n*t)));
 	}
 }
@@ -70,6 +74,59 @@ class Bank
 	{
 		Scanner sc=new Scanner(System.in);
 		char type;
-		System.out.println("Enter the type of account you want to create, s for saving ");
+		System.out.println("Enter the type of account you want to create, s for saving and c for current account");
 		type=sc.nextChar();
-		if(type==
+		if(type=='s')
+		{
+			Sav_acct s1=new Sav_acct();
+			s1.get_values();
+			int c;
+			
+			while(true)
+			{
+				System.out.println("1.Deposit\n2.Display\n3.Withdraw\n4.Interest\n5.Exit");
+				System.out.println("Enter your choice");
+				c=sc.nextInt();
+				switch(c)
+				{
+					case 1:s1.depo();
+						break;
+					case 2:s1.display_bal();
+						break;
+					case 3:s1.withdraw();
+						break;
+					case 4:s1.interest();
+						break;
+					default:System.exit(0);
+				}
+			}
+		}
+		else if(type=='c')
+		{
+			
+			Curr_acct c1=new Curr_acct();
+			c1.get_values();
+			int c;
+			
+			while(true)
+			{
+				System.out.println("1.Deposit\n2.Display\n3.Withdraw\n4.Exit");
+				System.out.println("Enter your choice");
+				c=sc.nextInt();
+				switch(c)
+				{
+					case 1:c1.depo();
+						break;
+					case 2:c1.display_bal();
+						break;
+					case 3:c1.withdraw();
+						break;
+					default:System.exit(0);
+					
+				}
+			}
+		}
+		else
+			System.out.println("Invalid choice");
+	}
+}
